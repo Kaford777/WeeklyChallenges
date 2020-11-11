8d3ddafb77eb03c8622a379bc6ed29fad200f457
kaford777 <westvalley2@gmail.com>
2020-11-11 07:04:20 -0800

    Weekly Challenges 01 done

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -2,8 +2,12 @@
 
 namespace ChallengesWithTestsMark8
 {
+
+    
     public class ChallengesSet01
     {
+        
+
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
@@ -18,32 +22,53 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException(); //delete this line 
+            //finish the method here
+            return minuend - subtrahend;
+
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            // delete this line   throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return factor1  * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+
+            else
+               return ($"Hello, {nameOfPerson}!");
+            
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            return "HEY!";
         }
     }
 }
